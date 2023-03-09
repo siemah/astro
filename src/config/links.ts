0bbe71bcf78229e6ad6e802db5157587bbd56fca
siemah <hmi.dayenio@gmail.com>
2023-03-09 17:41:18 +0100

    feat:show product details

--- src/config/links.ts
+++ src/config/links.ts
@@ -4,6 +4,7 @@ const globalLinks = {
   website: `https://${env.WC_API_DEPLOY_URL}/wp-json/zzenz/v1/store/${env.VENDOR_ID}/website`,
   shipping: `https://${env.WC_API_DEPLOY_URL}/wp-json/zzenz/v1/store/${env.VENDOR_ID}/shipping/list`,
   products: `https://${env.WC_API_DEPLOY_URL}/wp-json/wc/v3/store/${env.VENDOR_ID}/products`,
+  apiProducts: `https://${env.WC_API_DEPLOY_URL}/wp-json/wc/v3/products`,
   productsCategories: `https://${env.WC_API_DEPLOY_URL}/wp-json/wc/v3/store/${env.VENDOR_ID}/products/categories`,
 };
 export default globalLinks;
--- src/helpers/services/products/index.ts
+++ src/helpers/services/products/index.ts
@@ -6,9 +6,14 @@ import httpRequest from "../../http";
  * 
  * @returns list of store products
  */
-export default async function getProducts() {
+export default async function getProducts(searchParams?: string | string[][] | Record<string, string> | URLSearchParams) {
+  const search = new URLSearchParams(searchParams);
+  const endpoint = !!searchParams
+    ? `${globalLinks.apiProducts}?${search}`
+    : globalLinks.products;
+    !!searchParams && console.log("<<<<<[[[[[[[[[[[[endpoint]]]]]]]]]]]]>>>>>", endpoint)
   const products = await httpRequest({
-    url: globalLinks.products
+    url: endpoint
   });
 
   return products;
