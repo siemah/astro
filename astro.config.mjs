a2a3708415fff8668270e9ae16e6c6c020603f03
siemah <hmi.dayenio@gmail.com>
2023-03-09 17:45:30 +0100

    conf:show env vars

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
-
+console.log(process.env)
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  site: process.env.DOMAIN,
+  // site: process.env.DOMAIN,
   integrations: [
     image({
       serviceEntryPoint: "@astrojs/image/sharp"
