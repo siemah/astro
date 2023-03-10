2c26e28593bcf3e44bda1597f4e8d6960b1e0c08
siemah <hmi.dayenio@gmail.com>
2023-03-10 04:51:56 +0100

    conf:use purgecss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
     image({
       serviceEntryPoint: "@astrojs/image/sharp"
     }),
+    /**
+     * please add this at the end 
+     * @see https://github.com/codiume/orbit/tree/main/packages/astro-purgecss#-configuration
+     */
+    purgecss()
   ]
 });
