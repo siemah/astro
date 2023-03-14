88e8ea1db241045164c43de4ca43a5ee9aa3c3c7
siemah <hmi.dayenio@gmail.com>
2023-03-14 10:22:52 +0100

    test:use default font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import purgecss from "astro-purgecss";
-import fonts from "astro-fonts-next";
+// import fonts from "astro-fonts-next";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,9 +11,9 @@ export default defineConfig({
     image({
       serviceEntryPoint: "@astrojs/image/sharp"
     }),
-    fonts({
-      url: "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
-    }),
+    // fonts({
+    //   url: "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
+    // }),
     /**
      * please add this at the end 
      * @see https://github.com/codiume/orbit/tree/main/packages/astro-purgecss#-configuration
