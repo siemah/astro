fb9590eabac0e8d77c9c3cfb3874e857d5a21818
siemah <hmi.dayenio@gmail.com>
2023-03-16 15:27:06 +0100

    feat:generate sitemap &add a link tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import purgecss from "astro-purgecss";
-// import fonts from "astro-fonts-next";
+import fonts from "astro-fonts-next";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,13 +13,19 @@ export default defineConfig({
     image({
       serviceEntryPoint: "@astrojs/image/sharp"
     }),
-    // fonts({
-    //   url: "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
-    // }),
+    fonts({
+      url: "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap"
+    }),
     /**
      * please add this at the end 
      * @see https://github.com/codiume/orbit/tree/main/packages/astro-purgecss#-configuration
      */
-    purgecss()
+    purgecss(),
+    sitemap({
+      entryLimit: 450000,
+      changefreq: "daily",
+      priority: 0.9,
+      lastmod: new Date(),
+    })
   ]
 });
