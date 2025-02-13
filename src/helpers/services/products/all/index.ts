1a29b1cfe69744fb30a5e84713d5180cb4f9dbcc
siemah <hmi.dayenio@gmail.com>
2025-02-14 00:43:02 +0100

    ref:fetch everything

--- src/helpers/services/products/all/index.ts
+++ src/helpers/services/products/all/index.ts
@@ -11,7 +11,6 @@ interface ParamsType {
  * @returns list of store products
  */
 export default async function getAllProducts() {
-  const max_page = 5;// means 5 * per_page = 120 products
   const products: Record<string, any>[] = [];
   const per_page = 24;
   let isNotDone = true;
@@ -27,7 +26,7 @@ export default async function getAllProducts() {
     });
     products.push(...productsPerPage as []);
 
-    if (productsPerPage?.length === 0 || productsPerPage?.length < per_page || max_page <= page) {
+    if (productsPerPage?.length === 0 || productsPerPage?.length < per_page) {
       isNotDone = false;
     }
   }
@@ -36,7 +35,6 @@ export default async function getAllProducts() {
 }
 
 export async function getProductsOfCategory(categoryId: number | string) {
-  const max_page = 4;// means 5 * per_page = 120 products
   const products = [];
   const per_page = 24;
   let isNotDone = true;
@@ -53,7 +51,7 @@ export async function getProductsOfCategory(categoryId: number | string) {
     });
     products.push(...productsPerPage as []);
 
-    if (productsPerPage?.length === 0 || productsPerPage?.length < per_page || max_page <= page) {
+    if (productsPerPage?.length === 0 || productsPerPage?.length < per_page) {
       isNotDone = false;
     }
   }
